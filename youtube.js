670b932225a442bbc1fffeaac138a9941cc3ad40
Brandon B <broe5392@yahoo.com>
2018-07-07 11:47:21 -0400

    basic youtube.js creation

---
+++ youtube.js
@@ -0,0 +1,6 @@
+var apikey = "AIzaSyCFQH5Y6jt67hPbKz1lw95jwjgFYGxG_QA"
+var game = "fortnite"
+var queryURL = "https://www.youtube.com/results?search_query="+ game +"+gameplay&api_key=" + apikey
+
+
+console.log(queryURL);
